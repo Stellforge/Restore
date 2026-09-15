a2be4b52876aa8ffd2107401ea397661e8468943
Furkan <ercelebifurkan@gmail.com>
2026-09-15 22:37:11 +0300

    ..

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08a5d79cffb4b01bc327d708e87c1558c8466986")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c253d9adc0833fece180895aac4391973b4c98")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
